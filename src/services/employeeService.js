a9d25cda6157fd4b21c2834e157b456bffa77224
nanthessh <134475568+nanthessh@users.noreply.github.com>
2025-09-13 13:10:51 +0530

    Update employeeService.js
    Check for git

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://localhost:7071/api/employee';
-
+console.log("Check Git Change")
 export const getEmployees = () => axios.get(API_URL);
 export const addEmployee = (employee) => axios.post(API_URL, employee);
 export const updateEmployee = (employee) => axios.put(`${API_URL}/${employee.id}`, employee);
